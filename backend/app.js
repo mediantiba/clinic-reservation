fa8260fa0d58e859e469faacc3121e94a51543e4
Median Tiba <median.tiba@absolutesys.com>
2021-08-31 11:01:06 +0300

    Update ports

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:4200', 'http://localhost']
+  origin: ['http://localhost:4200', 'http://localhost:3001']
 }));
 
 const doctorsRouter = require("./routers/doctors");
